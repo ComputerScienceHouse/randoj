4b49e4953a2cdff2fdc6e533621c7630488908e5
Cecilia Lau <cecilialau6776@gmail.com>
2023-10-04 19:26:46 -0400

    nginx

--- local_settings.py
+++ local_settings.py
@@ -17,7 +17,7 @@ HOST = os.environ.get("HOST", "")
 
 # Uncomment and set to the domain names this site is intended to serve.
 # You must do this once you set DEBUG to False.
-ALLOWED_HOSTS = [HOST]
+ALLOWED_HOSTS = ["localhost", HOST]
 
 # Optional apps that DMOJ can make use of.
 INSTALLED_APPS += ()
